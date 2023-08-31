a4d6edc28e3dac6b7865d4350d5a5a47d7fcf0a3
Felipe Garcia <felipe.phellipe@gmail.com>
2023-08-31 13:43:26 +0100

    remove scrape task from main

--- scraper/src/main/java/com/felipe/Main.java
+++ scraper/src/main/java/com/felipe/Main.java
@@ -1,21 +1,37 @@
 package com.felipe;
 
+import com.felipe.models.Scraping.ScrapperElement;
 import com.felipe.models.Site;
+import com.felipe.models.grocery.GroceryProduct;
+import com.felipe.repository.GroceryRepository;
 import com.felipe.tasks.ScrapingTask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
+/**
+ * div/pwc-tile--price-secondary
+ * 	span/pwc-m-unit -> unit
+ * 	span/ct-price-value -> priceperunit
+ *
+ * div/pwc-tile--price-primary
+ * 	span/ct-price-formatted -> price
+ * 	span/pwc-m-unit -> unit
+ *
+ * div/col-tile--brand-wrapper
+ * 	p/pwc-tile--quantity -> quantity
+ * 	p/pwc-tile--brand -> brand
+ */
 @SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = GroceryRepository.class)
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException {
         SpringApplication.run(Main.class, args);
-        Site site = new Site();
-        site.setUrl("https://www.continente.pt/mercearia/?start=0&srule=FOOD_Mercearia&pmin=0.01");
-        ExecutorService service = Executors.newCachedThreadPool();
-        service.execute(new ScrapingTask(site));
+
     }
 }
