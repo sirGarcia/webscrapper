c98184f6837f227b28ddd304aa96cc3eb93ab822
Felipe Garcia <felipe.phellipe@gmail.com>
2023-08-31 13:46:06 +0100

    task now have repository for storing products and fully dynamic scrape elements

--- scraper/src/main/java/com/felipe/tasks/ScrapingTask.java
+++ scraper/src/main/java/com/felipe/tasks/ScrapingTask.java
@@ -1,37 +1,56 @@
 package com.felipe.tasks;
 
 import com.felipe.models.Site;
+import com.felipe.models.grocery.GroceryProduct;
+import com.felipe.repository.GroceryRepository;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ReflectionUtils;
 
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.List;
 
+@Slf4j
 public class ScrapingTask implements Runnable{
 
     private final Site site;
     private final WebClient webClient = new WebClient();
 
-    public ScrapingTask(Site site) {
+    private final GroceryRepository repository;
+
+    public ScrapingTask(Site site, GroceryRepository repository) {
         this.site = site;
-        webClient.getOptions().setCssEnabled(false);
-        webClient.getOptions().setJavaScriptEnabled(false);
+        this.repository = repository;
     }
 
     @Override
     public void run() {
         System.out.println("started");
+        webClient.getOptions().setCssEnabled(false);
+        webClient.getOptions().setJavaScriptEnabled(false);
         try {
             HtmlPage page = webClient.getPage(site.getUrl());
-            List<HtmlElement> list = page.getByXPath("//div[@class=\"product\"]");
-            System.out.println("List size " + list.size());
+            List<HtmlElement> list = page.getByXPath(site.getGridElementClass());
+            log.info("grid items size: {}", list.size());
             list.forEach(htmlElement -> {
-                //List<HtmlElement> anchor = htmlElement.getByXPath("//div[contains(@class, \"ct-tile-body\")]");
-                System.out.println("---------------------------------------------------------------------------------");
+                GroceryProduct product = new GroceryProduct();
                 System.out.println(htmlElement.asXml());
+                site.getListToMap().forEach(scrapperElement -> {
+                    try {
+                        HtmlElement innerElement = htmlElement.getFirstByXPath(scrapperElement.getXpath());
+                        Field curField = product.getClass().getDeclaredField(scrapperElement.getField());
+                        curField.setAccessible(true);
+                        ReflectionUtils.setField(curField, product, innerElement.asNormalizedText());
+                    } catch (NoSuchFieldException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+                System.out.println(product.toString());
+                repository.save(product);
             });
         } catch (IOException e) {
             throw new RuntimeException(e);
